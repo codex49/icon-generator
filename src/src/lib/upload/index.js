1bb384a55d64d48ce3003c950d174007dad16f4e
ahmed.ziani <ahmed.ziani@ext.cdiscount.com>
2017-04-07 17:59:03 +0200

    add function update

---
+++ src/src/lib/upload/index.js
@@ -0,0 +1,10 @@
+export default  (link, elem) => {
+    const fReader = new FileReader();
+
+    fReader.readAsDataURL(link.files[0]);
+
+    $(fReader).on('loadend', function(){
+        const img = $(elem).find('img')[0];
+        img.src = event.target.result;
+    });
+}
