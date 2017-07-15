9e91fbc1491f1aff7c8918e4acfedfe334b84003
ahmed ziani <ahmed ziani>
2017-07-15 16:30:12 +0100

    update backgrounds component

--- src/components/Backgrounds/index.js
+++ src/components/Backgrounds/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Background from './components/Background';
 import BackgroundUpload from './components/BackgroundUpload';
@@ -7,40 +7,37 @@ import TitleCategory from '../TitleCategory';
 
 import uploadIcon from '../../public/assets/img/icons/upload.png';
 
-export default class Backgrounds extends Component {
-    renderListCategory = () => {
-        return (
-            this.props.listCategories.map(category => {
-                const items = category.items.map( (item, f) => {
-                    const background = [];
+const renderBackground = category => {
+    const items = category.items.map( (item, f) => {
+        const background = [];
 
-                    if(category.name === 'Gradients' && f === 0) {
-                         background.push( <CustomGradient /> );
-                     }
-                    background.push(<Background key={f} link={item}/>);
-                    return background;
-                });
-                return (
-                    <li key={category.name} className="category">
-                        <TitleCategory link={category.iconCat}>{category.name}</TitleCategory>
-                        <ul className="items">{items}</ul>
-                    </li>
-                );
-            })
-        );
-    }
-    render () {
-        return (
-            <div className="nav-element backgrounds">
-                <h2 className="title-categories">Backgrounds</h2>
-                <ul className="categories">
-                    <BackgroundUpload
-                        active='active'
-                        link={uploadIcon}
-                    />
-                    {this.renderListCategory()}
-                </ul>
-            </div>
-        );
-    }
-}
+        if(category.name === 'Gradients' && f === 0) {
+            background.push( <CustomGradient /> );
+        }
+        background.push(<Background key={f} link={item}/>);
+        return background;
+    });
+    return (
+        <li key={category.name} className="category">
+            <TitleCategory link={category.iconCat}>{category.name}</TitleCategory>
+            <ul className="items">{items}</ul>
+        </li>
+    );
+};
+
+const renderListCategory = props => props.listCategories.map(renderBackground);
+
+const Backgrounds = props => (
+    <div className="nav-element backgrounds">
+        <h2 className="title-categories">Backgrounds</h2>
+        <ul className="categories">
+            <BackgroundUpload
+                active='active'
+                link={uploadIcon}
+            />
+            {renderListCategory(props)}
+        </ul>
+    </div>
+);
+
+export default Backgrounds;
